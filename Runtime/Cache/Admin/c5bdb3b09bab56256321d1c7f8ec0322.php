eb6f5c025118c5c5c32694d86a6815166a88ea55
kuba8 <kuba8@qq.com>
2016-12-04 21:46:56 +0800

    23

--- Runtime/Cache/Admin/c5bdb3b09bab56256321d1c7f8ec0322.php
+++ Runtime/Cache/Admin/c5bdb3b09bab56256321d1c7f8ec0322.php
@@ -158,7 +158,16 @@
             </table>
             <!--商品属性-->
             <table style="display: none;" width="90%" class="tab_table" align="center">
-            
+              <tr>
+                    <td>
+                        商品类型：<?php buildSelect('Type','type_id','id','type_name',$data['type_id']);?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <ul id='attr_list'></ul>
+                    </td>
+                </tr>
             </table>
             <!--商品相册-->
             <table style="display: none;" width="90%" class="tab_table" align="center">
@@ -242,7 +251,59 @@ $(".btn_del_pic").click(function(){
     }
 });
 
+    $("select[name=type_id]").change(function(){
+        var typeId=$(this).val();
+        if(typeId>0)
+        {
+            $.ajax({
+                type:"GET",
+                url:"<?php echo U('ajaxGetAttr','',FALSE);?>/type_id/"+typeId,
+                dataType:"json",
+                success:function(data)
+                {
+                    var li="";
+                    $(data).each(function(k,v){
+                        li +='<li>';
+                        if(v.attr_type=='可选')
+                            li+='<a onclick="addNewAttr(this);" href="#">[+]</a>';
+                        li+=v.attr_name+'：';
+                        if(v.attr_option_values=="")
+                            li+='<input type="text" name="attr_value['+v.id+'][]"/>';
+                        else
+                        {
+                            li+='<select name="attr_value['+v.id+'][]"><option value="">请选择...</option>';
+                            var _attr=v.attr_option_values.split(',');
+                            for(var i=0;i<_attr.length;i++)
+                            {
+                                li+='<option value="'+_attr[i]+'">';
+                                li+=_attr[i];
+                                li+='</option>';
+                            }
+                            li+='</select>';
+                        }
+                        li+='</li>'
+                    });
+                    $("#attr_list").html(li);
+                }
+            });
+        }
+        else
+            $("#attr_list").html("");
+        });
 
+        function addNewAttr(a)
+        {
+            var li = $(a).parent();
+            if($(a).text()=='[+]')
+            {
+                var newLi = li.clone();
+                newLi.find("a").text('[-]');
+                li.after(newLi);
+            }
+            else
+                li.remove();
+        }
+    </script>
     </script>
 
 
